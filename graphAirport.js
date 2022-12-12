5c60fddaced3de01531d023b19769f322c6c3333
vishalkrsoni <vishal.sony1@gmail.com>
2022-12-12 12:01:53 +0530

    graph implementation

---
+++ graphAirport.js
@@ -0,0 +1,39 @@
+const airportList = 'A B C D E F G H I J K'.split(' ')
+const routes = [
+  ['A', 'D'],
+  ['A', 'E'],
+  ['D', 'C'],
+  ['D', 'B'],
+  ['D', 'H'],
+  ['F', 'I'],
+  ['F', 'E'],
+  ['F', 'B'],
+  ['F', 'J'],
+  ['K', 'B'],
+  ['C', 'I']
+]
+
+// taking a Map for Graph representation
+const routesGraph = new Map();
+
+
+// Adding all the nodes
+const addNode = (airport) => {
+  routesGraph.set(airport, [])
+
+}
+
+// Making connections
+const addEdgeNode = (source, destination) => {
+  routesGraph.get(source).push(destination)
+  routesGraph.get(destination).push(source)
+}
+
+// calling functions 
+airportList.forEach(addNode)
+routes.forEach(route => addEdgeNode(...route))
+
+console.log(routesGraph)
+
+// BFS traversal
+
